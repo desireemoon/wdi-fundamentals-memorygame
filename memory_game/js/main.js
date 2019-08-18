ede124bc9311e7f2b5ce34a8511922d7d52f58d4
Desiree deWysocki <desireedewysocki@Desirees-MacBook-Air.local>
2019-08-18 12:10:09 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,28 +1,20 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-var cardThree = cards[2];
-cardsInPlay.push(cardThree);
-
-var cardFour = cards[3];
-cardsInPlay.push(cardFour);
-
-console.log("user flipped " + cardsInPlay[0]);
-console.log("user flipped " + cardsInPlay[1]);
-console.log("user flipped " + cardsInPlay[2]);
-console.log("user flipped " + cardsInPlay[3]);
-
-if (cardsInPlay.length === 4) {
-    if (cardsInPlay[2] === cardsInPlay[3]) {
+var checkForMatch = function () {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
         alert("You found a match!");
     } else {
         alert("Sorry, try again.");
     }
-    
 }
+
+
+var flipCard = function (cardId) {
+    checkForMatch();
+    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+}
+
+flipCard(0);
+flipCard(2);
