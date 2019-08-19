acd4dba83160d5bafb5e5a7f11e2453012172066
Desiree deWysocki <desireedewysocki@Desirees-MacBook-Air.local>
2019-08-18 22:28:16 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -37,12 +37,26 @@ var checkForMatch = function () {
 
 //checks pair then flips card
 var flipCard = function (cardId) {
+   var cardId = this.getAttribute('data-id');
     console.log("User flipped " + cards[cardId].rank);
     cardsInPlay.push(cards[cardId].rank);
     console.log(cards[cardId].cardImage);
     console.log(cards[cardId].suit);
-    checkForMatch();
+    this.setAttribute('src', cards[cardId].cardImage);
+    if(cardsInPlay.length === 2) {
+        checkForMatch();
+    }
 }
 
-flipCard(0);
-flipCard(2);
+//creates board and sets up game
+var createBoard = function () {
+    for(var i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i); 
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+    };
+};
+
+createBoard();
